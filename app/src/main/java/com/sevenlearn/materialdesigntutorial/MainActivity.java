25cc086ef4ae8e0582fc89a8e0c63d4911d860b4
Saeed Shahini <saeedshahiniit@gmail.com>
2020-02-06 12:17:35 +0330

    first commit

---
+++ app/src/main/java/com/sevenlearn/materialdesigntutorial/MainActivity.java
@@ -0,0 +1,16 @@
+package com.sevenlearn.materialdesigntutorial;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar_main));
+    }
+}
