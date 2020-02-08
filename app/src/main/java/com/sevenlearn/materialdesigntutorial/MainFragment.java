212e766a0a05715b7a84c151c8eaffd8da3d2cb2
Saeed Shahini <saeedshahiniit@gmail.com>
2020-02-08 19:45:36 +0330

    working on it

--- app/src/main/java/com/sevenlearn/materialdesigntutorial/MainFragment.java
+++ app/src/main/java/com/sevenlearn/materialdesigntutorial/MainFragment.java
@@ -8,6 +8,10 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.google.android.material.card.MaterialCardView;
+
 
 public class MainFragment extends Fragment {
 
@@ -15,5 +19,27 @@ public class MainFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_main,container,false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        final MaterialCardView materialCardView=view.findViewById(R.id.card_main_text);
+        materialCardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FragmentTransaction transaction=getActivity().getSupportFragmentManager().beginTransaction();
+                transaction.add(R.id.frame_main_fragmentContrainer,new DetailFragment());
+                transaction.addToBackStack(null);
+                transaction.commit();
+            }
+        });
+//        materialCardView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                materialCardView.setChecked(!materialCardView.isChecked());
+//            }
+//        });
     }
 }
